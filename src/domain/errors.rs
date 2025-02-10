8f0b55610f2294728d0ae03b21ec2b74ecf79eb9
Felipe Torres González <torresfelipex1@gmail.com>
2025-02-10 13:39:18 +0100

    Add new error type and derive from std::error::Error

--- src/domain/errors.rs
+++ src/domain/errors.rs
@@ -6,15 +6,29 @@
 
 //! Module with definitions for custom error types.
 
+use thiserror::Error;
+
 /// Error types for the CNMV handler.
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum CnmvError {
     /// Error given when the passed company is not recognized by the CNMV' API.
+    #[error("")]
     UnknownCompany,
     /// Error from the external API (CNMV).
+    #[error("")]
     ExternalError(String),
     /// Error for the internal methods.
+    #[error("")]
     InternalError(String),
     /// CNMV identifies companies using ISIN.
+    #[error("")]
     MissingIsin,
+}
+
+#[derive(Error, Debug)]
+pub enum DbError {
+    #[error("missing stock information in the DB")]
+    MissingStockInfo(String),
+    #[error("unknown db error")]
+    Unknown(String),
 }
